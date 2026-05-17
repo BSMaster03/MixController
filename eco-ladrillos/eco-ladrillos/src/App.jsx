7fa3f91811554e2bc5a0a05e67cb8d3a14b6c123
Sebastián Ciniglio <sebasciniglio@gmail.com>
2026-05-17 13:51:20 -0400

    Add files via upload

---
+++ eco-ladrillos/eco-ladrillos/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./pages/Menu";
+import CreateProject from "./pages/CreateProject";
+import SavedProjects from "./pages/SavedProjects";
+import ModifyProjects from "./pages/ModifyProjects";
+import EditProject from "./pages/EditProject";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/app" element={<Menu />} />
+        <Route path="/app/proyecto" element={<CreateProject />} />
+        <Route path="/app/proyectos" element={<SavedProjects />} />
+        <Route path="/app/modificar" element={<ModifyProjects />} />
+        <Route path="/app/modificar/:id" element={<EditProject />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
