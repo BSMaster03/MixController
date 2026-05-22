a6f7911b4ccfdd39048b0ca12df2f77ca7e11ca9
Sebastián Ciniglio <sebasciniglio@gmail.com>
2026-05-22 17:48:46 -0400

    Add route for viewing a specific project
    Update file containing the path for exporting all the required pages

--- eco-ladrillos/eco-ladrillos/src/App.jsx
+++ eco-ladrillos/eco-ladrillos/src/App.jsx
@@ -4,6 +4,7 @@ import CreateProject from "./pages/CreateProject";
 import SavedProjects from "./pages/SavedProjects";
 import ModifyProjects from "./pages/ModifyProjects";
 import EditProject from "./pages/EditProject";
+import ViewProject from "./pages/ViewProject";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/app" element={<Menu />} />
         <Route path="/app/proyecto" element={<CreateProject />} />
         <Route path="/app/proyectos" element={<SavedProjects />} />
+        <Route path="/app/ver/:id" element={<ViewProject />} />
         <Route path="/app/modificar" element={<ModifyProjects />} />
         <Route path="/app/modificar/:id" element={<EditProject />} />
       </Routes>
